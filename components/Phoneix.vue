90ed0b9e57da698dd0d947af6f5e5f38edb7a194
Eugene Mayer <white@pfel.ru>
2021-07-25 10:42:01 +0300

    Уже можно вводить. Неудобно, но можно.

--- components/Phoneix.vue
+++ components/Phoneix.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="phoneix_box">
-    <input type="text" class="phoneix_input placeholder" v-model="placeholder" :style="{'color': color_ph}">
+    <input type="text" class="phoneix_input placeholder" v-model="placeholder" :style="{'color': color_ph}" @focus="$refs.phoneix_input.focus()">
     <input type="tel" class="phoneix_input" ref="phoneix_input" value="+7 (" v-model="phone" @input="input" autofocus :style="{'color': color}">
   </div>
 </template>
@@ -17,22 +17,53 @@ export default {
     color_ph: {
       type: String,
       default: '#595959'
+    },
+    base: {
+      type: String,
+      default: '7'
     }
   },
   data() {
     return {
-      phone: "+7 (",
-      ideal: "+7 (000) 000-00-00",
+      phone: `+${this.base} (`,
+      ideal: `+${this.base} (000) 000-00-00`,
+      phone_history: [`+${this.base} (`],
+      history_position: 0,
+      old_position: 4,
     }
   },
   methods: {
     input: function (e) {
 
+      this.phone = this.formatted_phone()
+      this.phone_history.push(this.phone)
+      this.history_position += 1
+    },
+    formatted_phone: function (adding=true) {
+      let f3 = this.raw.substr(1, 3) // Первые три цифры -- (999)
+      let s3 = this.raw.substr(4, 3) // Вторые три цифры
+      let f2 = this.raw.substr(7, 2) // Первые две цифры из последнего блока из четырёх цифр
+      let s2 = this.raw.substr(9, 2) // Вторые две цифры
+
+      let normal = `+${this.base} (`
+
+      if(f3.length) normal = `+${this.base} (${f3}${adding && f3.length === 3 ? ") " : ""}`
+      if(s3.length) normal = `+${this.base} (${f3}) ${s3}${adding && s3.length === 3 ? "-" : ""}`
+      if(f2.length) normal = `+${this.base} (${f3}) ${s3}-${f2}${adding && f2.length === 2 ? "-" : ""}`
+      if(s2.length) normal = `+${this.base} (${f3}) ${s3}-${f2}-${s2}`
+
+      return normal
     }
   },
   computed: {
     placeholder: function () {
       return  this.phone + this.ideal.substr(this.phone.length)
+    },
+    raw: function () {
+      return this.phone.replace( /\D+/g, '')
+    },
+    old: function () {
+      return this.phone_history[-1]
     }
   }
 }
